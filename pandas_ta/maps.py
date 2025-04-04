c5c43c7bc1526dea400df3f56143237ad8b415ec
ff137 <ff137@proton.me>
2025-04-04 09:37:58 +0200

    :art: Modify distribution name
    Signed-off-by: ff137 <ff137@proton.me>

--- pandas_ta/maps.py
+++ pandas_ta/maps.py
@@ -5,7 +5,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 
 from pandas_ta._typing import Dict, IntFloat, ListStr
 
-_dist = get_distribution("pandas_ta")
+_dist = get_distribution("pandas_ta_ff137")
 try:
     # Normalize case for Windows systems
     _here = Path(_dist.location) / __file__
