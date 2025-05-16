d7125afd23f26fec2573663c0c117fe5124008c5
ff137 <ff137@proton.me>
2025-05-17 00:06:18 +0300

    :bookmark: Release 0.5.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "5", "1")),
+    version=".".join(("0", "5", "2")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
